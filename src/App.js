cb772dfb1e3ed7842e59bf85b1e4eab725bd3023
fkuca <fkuca91@gmail.com>
2021-03-14 22:30:58 +0100

    Add Login component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import "./App.css";
 import Login from "./components/login/Login";
 
 function App() {
--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,7 +1,58 @@
-import * as React from "react";
+import React, { useState } from "react";
+import "./Login.css";
 
 const Login = () => {
-  return <div>Login</div>;
+  const [error, setSerror] = useState();
+
+  const onLoadingCharacter = () => {
+    console.log("onLoadingCharacter");
+  };
+
+  const onCreatingNewCharacter = () => {
+    console.log("onCreatingNewCharacter");
+  };
+
+  const buttons = [
+    {
+      text: "Load character",
+      class: "button--negative",
+      function: onLoadingCharacter,
+    },
+    {
+      text: "Create new character",
+      class: "button--positive",
+      function: onCreatingNewCharacter,
+    },
+  ];
+
+  return (
+    <div className="login">
+      <div className="login__background" />
+      <div className="login__header">
+        <h1>Character creator</h1>
+      </div>
+      <div className="login__buttons">
+        <input
+          className="input"
+          name="characterCode"
+          placeholder="Enter character code"
+        />
+        {buttons.map((button) => (
+          <button
+            key={`button-${button.text}`}
+            className={`button ${button.class}`}
+            onClick={button.function}
+          >
+            {button.text}
+          </button>
+        ))}
+      </div>
+      <div className="login__errors">{error}</div>
+      <div className="login__copyrights">
+        &#169; Ruljin & Ulwothar, All rights reserved
+      </div>
+    </div>
+  );
 };
 
 export default Login;
