f60fef4c6fd0d3521e745e9695dc0f79a4a2aa24
fkuca <fkuca91@gmail.com>
2021-03-14 22:51:04 +0100

    Add Auth component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import "./App.css";
-import Login from "./components/login/Login";
+import Auth from "./pages/auth/Auth";
 
 function App() {
-  return <Login />;
+  return <Auth />;
 }
 
 export default App;
---
+++ src/pages/auth/Auth.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Login from "../../components/login/Login";
+import "./Auth.css";
+
+const Auth = () => {
+  return (
+    <div className="auth">
+      <Login />
+    </div>
+  );
+};
+
+export default Auth;
