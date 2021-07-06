749932d3b75839afbb44b2b5decfb13a97946ee3
fkuca <fkuca91@gmail.com>
2021-07-07 00:16:10 +0200

    feat: add new styles and refactor Column and Row components

--- src/components/row/Row.js
+++ src/components/row/Row.js
@@ -1,10 +1,14 @@
 import PropTypes from "prop-types";
+import clsx from "clsx";
 import styles from "./row.module.css";
 
-export const Row = ({ children }) => {
-  return <div className={styles.row}>{children}</div>;
+export const Row = ({ children, className }) => {
+  const classes = clsx(styles.row, className);
+
+  return <div className={classes}>{children}</div>;
 };
 
 Row.propTypes = {
   children: PropTypes.node.isRequired,
+  className: PropTypes.string,
 };
