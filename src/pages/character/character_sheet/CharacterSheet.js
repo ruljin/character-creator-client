e43ff584c98b973dce7697cd024d1cf23ac23faa
fkuca <fkuca91@gmail.com>
2021-07-06 22:00:55 +0200

    feat: add CharachterSheet component

--- src/pages/character/character_sheet/CharacterSheet.js
+++ src/pages/character/character_sheet/CharacterSheet.js
@@ -1,7 +1,7 @@
 import { General } from "./sections/general/General";
 import { Statistics } from "./sections/statistics/Statistics";
-import { Skills } from "./sections/statistics/Statistics";
-import { Spells } from "./sections/statistics/Statistics";
+import { Skills } from "./sections/skills/Skills";
+import { Spells } from "./sections/spells/Spells";
 
 export const CharacterSheet = () => {
   return (
