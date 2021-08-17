7ded4e238b82622cc1145582d4e230c1412f5bee
Filip <fkuca91@gmail.com>
2021-08-17 19:33:56 +0200

    Create new character (#4)
    * Add authorization context, getFromStorage, Layout and Page components, refactor App and Auth components
    
    * Refactor useAuthorization hook
    
    * Add character, character-sheet components mockup and new styling
    
    * fix(login): remove old login file after merge
    
    * feat(login): add authentication using AuthorizationContext
    
    * feat(thematic_break): add ThematicBreak component
    
    * feat(character): add Character, CharacterSheet, General, Skills, Spells and Statistics components
    
    * feat: add Column and Row components
    
    * feat: add CharachterSheet component
    
    * feat: add Skills, Spells, Statistics and General components
    
    * feat: add variables.css
    
    * feat: add new styles and refactor Column and Row components
    
    * feat: add General inputs styles General
    
    * feat: add Statistics component with inputs and styles
    
    * feat: add Tooltip component
    
    * fix: change file format to JSX and input type to number
    
    * fix: remove Weight.js file
    
    * fix(ThematicBreak): add new .css variable, change ThematicBreak file extension from .js to .jsx
    
    * fix: change files format extension from .js to .jsx
    
    * feat: add Skills with inputs
    
    * feat: add Spells with inputs
    
    * feat(button): add button label variant
    
    * feat: pass skills and spells as props to children components
    
    * chore: change components file extension from .js to .jsx
    
    * chore: add prettier to dev dependencies

--- src/pages/auth/Auth.jsx
+++ src/pages/auth/Auth.jsx
@@ -1,40 +1,40 @@
-import React, { useState, useContext } from "react";
-import { AuthorizationContext } from "../../context/authorizationContext";
-import { getCharacter } from "../../resources/characters/index";
-import { Login } from "./login/Login";
-import styles from "./auth.module.css";
+import React, { useState, useContext } from 'react';
+import { AuthorizationContext } from '../../context/authorizationContext';
+import { getCharacter } from '../../resources/characters/index';
+import { Login } from './login/Login';
+import styles from './auth.module.css';
 
 export const Auth = () => {
-  const { onSetCharacter } = useContext(AuthorizationContext);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
+	const { onSetCharacter } = useContext(AuthorizationContext);
+	const [loading, setLoading] = useState(false);
+	const [error, setError] = useState(false);
 
-  const handleGetCharacter = (code) => {
-    setLoading(true);
-    return new Promise((resolve) => {
-      getCharacter(code)
-        .then((response) => {
-          onSetCharacter(response.character);
-          setError(false);
-          setLoading(false);
-          resolve("OK");
-        })
-        .catch((error) => {
-          console.log("error", error);
-          setError(true);
-          setLoading(false);
-          resolve("error");
-        });
-    });
-  };
+	const handleGetCharacter = (code) => {
+		setLoading(true);
+		return new Promise((resolve) => {
+			getCharacter(code)
+				.then((response) => {
+					onSetCharacter(response.character);
+					setError(false);
+					setLoading(false);
+					resolve('OK');
+				})
+				.catch((error) => {
+					console.log('error', error);
+					setError(true);
+					setLoading(false);
+					resolve('error');
+				});
+		});
+	};
 
-  return (
-    <div className={styles.auth}>
-      <Login
-        loading={loading}
-        error={error}
-        handleGetCharacter={handleGetCharacter}
-      />
-    </div>
-  );
+	return (
+		<div className={styles.auth}>
+			<Login
+				loading={loading}
+				error={error}
+				handleGetCharacter={handleGetCharacter}
+			/>
+		</div>
+	);
 };
